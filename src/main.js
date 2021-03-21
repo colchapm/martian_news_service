d9b12719a66a0f51620b3ee7ff75d5cbb34c3e15
Collin Chapman <cchap14@gmail.com>
2021-03-21 15:48:49 -0700

    add user logic for inserting astronomy picture of the day
    Co-authored-by: HR Williams <halrubinwilliams@gmail.com>
    Co-authored-by: Saoud Rana <saoud@saoud.dev>

--- src/main.js
+++ src/main.js
@@ -10,10 +10,17 @@ import MarsConditions from './news-service.js';
 $(document).ready(function() {
   $("#marsWeather").click(function() {
     let promise = MarsConditions.getConditions();
+    let photoPromise = MarsConditions.getDailyPhoto();
     promise.then(function(response) {
       const body = JSON.parse(response);
       $(".showPressure").text(`The atmospheric pressure on Mars is ${body[body.sol_keys[3]]["PRE"].av}`);
       //$(".showWind").text(`The wind on Mars is ${codeherethatparsesjson}`);
+    }, function(error) {
+      $(".showErrors").text(`There was an error processing your request: ${error}`);
+    });
+    photoPromise.then(function(response) {
+      const photoBody = JSON.parse(response);
+      $(".showPhoto").html(`Here is the astronomy picture of the day <img src="${photoBody["url"]}" alt="${photoBody["title"]}">`);
     }, function(error) {
       $(".showErrors").text(`There was an error processing your request: ${error}`);
     });

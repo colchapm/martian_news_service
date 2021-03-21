f351bd827400ea1ebd6c125cdd96b97032f8a92e
Collin Chapman <cchap14@gmail.com>
2021-03-21 14:49:52 -0700

    create initial user interface logic
    Co-authored-by: HR Williams <halrubinwilliams@gmail.com>
    Co-authored-by: Saoud Rana <saoud@saoud.dev>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import MarsConditions from './news-service.js';
+
+
+
+
+$(document).ready(function() {
+  $("#marsWeather").click(function() {
+    let promise = MarsConditions.getConditions();
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $(".showPressure").text(`The atmospheric pressure on Mars is ${body[body.sol_keys[3]]["PRE"].av}`);
+      //$(".showWind").text(`The wind on Mars is ${codeherethatparsesjson}`);
+    }, function(error) {
+      $(".showErrors").text(`There was an error processing your request: ${error}`);
+    });
+  });
+});

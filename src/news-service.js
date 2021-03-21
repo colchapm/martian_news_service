4b4f131d0f5a08f4fe402a865520e62ddd0b5ef5
Collin Chapman <cchap14@gmail.com>
2021-03-21 15:49:14 -0700

    add business logic for inserting astronomy picture of the day
    Co-authored-by: HR Williams <halrubinwilliams@gmail.com>
    Co-authored-by: Saoud Rana <saoud@saoud.dev>

--- src/news-service.js
+++ src/news-service.js
@@ -14,4 +14,19 @@ export default class MarsConditions {
       request.send();
     });
   }
+  static getDailyPhoto() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+}
 }

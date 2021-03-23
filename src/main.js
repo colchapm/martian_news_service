b7def7378615aa12d75278075f29079fa67cb896
Collin Chapman <cchap14@gmail.com>
2021-03-22 18:56:28 -0700

    add mars rover photo of the day
    Co-authored-by: HR Williams <halrubinwilliams@gmail.com>
    Co-authored-by: Saoud Rana <saoud@saoud.dev>

--- src/main.js
+++ src/main.js
@@ -11,16 +11,23 @@ $(document).ready(function() {
   $("#marsWeather").click(function() {
     let promise = MarsConditions.getConditions();
     let photoPromise = MarsConditions.getDailyPhoto();
+    let marsPhotoPromise = MarsConditions.getDailyMarsPhoto();
     promise.then(function(response) {
       const body = JSON.parse(response);
-      $(".showPressure").text(`The atmospheric pressure on Mars is ${body[body.sol_keys[3]]["PRE"].av}`);
+      $(".showPressure").text(`The atmospheric pressure on Mars is ${body[body.sol_keys[0]]["PRE"].av}`);
       //$(".showWind").text(`The wind on Mars is ${codeherethatparsesjson}`);
     }, function(error) {
       $(".showErrors").text(`There was an error processing your request: ${error}`);
     });
     photoPromise.then(function(response) {
       const photoBody = JSON.parse(response);
-      $(".showPhoto").html(`Here is the astronomy picture of the day <img src="${photoBody["url"]}" alt="${photoBody["title"]}">`);
+      $(".showAstronomyPhoto").html(`Here is the astronomy picture of the day <img src="${photoBody["url"]}" alt="${photoBody["title"]}">`);
+    }, function(error) {
+      $(".showErrors").text(`There was an error processing your request: ${error}`);
+    });
+    marsPhotoPromise.then(function(response) {
+      const marsPhotoBody = JSON.parse(response);
+      $(".showRoverPhoto").html(`Here is the Mars Rover picture of the day <img src="${marsPhotoBody.photos[0].img_src}" alt="${marsPhotoBody["title"]}">`);
     }, function(error) {
       $(".showErrors").text(`There was an error processing your request: ${error}`);
     });
--- src/news-service.js
+++ src/news-service.js
@@ -29,4 +29,19 @@ export default class MarsConditions {
       request.send();
     });
 }
+  static getDailyMarsPhoto() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.API_KEY}`
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+}
 }
